1007a7e0851055ec111f72df3349e23011a395d6
Yuyadesu1020 <Puchiyongzai@gmail.com>
2025-08-04 21:57:01 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'common.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php include 'header.php'; ?>
+
+    <main>
+        <h1>Ashoonya yoga</h1>
+    </main>
+
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
+
+
+
+
+<!-- 前回のファイルが機能するとき -->
+<!-- lsof -i :3000 
+ ↓
+kill -9 13538-->
